d19b6b55b227fbd8c1304f60d97f3a8d81f84eff
denwolf4ever <denwolf4ever@gmail.com>
2024-04-30 15:06:19 +0300

    first commit

---
+++ Source/SnakeGame/SnakeBase.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SnakeBase.h"
+#include "SnakeElementBase.h"
+// Sets default values
+ASnakeBase::ASnakeBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	ElementSize = 100.f;
+	MovementSpeed = 10.f;
+	LastMoveDirection = EMovementDirection::UP;
+
+}
+
+// Called when the game starts or when spawned
+void ASnakeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, GetActorTransform());
+	AddSnakeElement(4);
+	
+}
+
+// Called every frame
+void ASnakeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Move(DeltaTime);
+
+}
+
+void ASnakeBase::AddSnakeElement(int ElementsNum)
+{
+	for (int i = 0; i < ElementsNum; ++i)
+	{
+		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
+		FTransform NewTransform = FTransform(NewLocation);
+		//SnakeElements.Num()*ElementSize;
+		ASnakeElementBase* NewSnakeElem = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform); 
+		NewSnakeElem->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
+		SnakeElements.Add(NewSnakeElem);
+	}
+	
+}
+
+void ASnakeBase::Move(float DeltaTime)
+{
+	FVector MovementVector(ForceInitToZero);
+	FVector MovementVector;
+	float MovementSpeedDelta = MovementSpeed * DeltaTime;
+
+	switch (LastMoveDirection)
+	{
+	case EMovementDirection::UP:
+		MovementVector.X += MovementSpeedDelta;
+		break;
+	case EMovementDirection::DOWN:
+		MovementVector.X -= MovementSpeedDelta;
+		break;
+	case EMovementDirection::LEFT:
+		MovementVector.Y += MovementSpeedDelta;
+		break;
+	case EMovementDirection::RIGHT:
+		MovementVector.Y -= MovementSpeedDelta;
+		break;
+	default:
+		break;
+	}
+	AddActorWorldOffset(MovementVector);
+}
+
